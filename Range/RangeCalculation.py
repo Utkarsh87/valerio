dd4786e4c0a460fb7bd2004555144fced75ee812
Utkarsh87 <utkarsh@Utkarsh.localdomain>
2020-05-20 15:23:56 +0530

    Added Range calculation script and Car details

---
+++ Range/RangeCalculation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('F:\\Valerio\\Scripts and Plots\\ValerioTrials\\Range\\VEHICLE_DATA.csv')
+
+car_list = list(df['VEHICLE NAME'])
+range_list = list(df['DRIVING RANGE (IN KM)'])
+
+def RemainingRange(battery_level):
+    print('Please select the Model: \n')
+    for car in car_list:
+        print(str(car_list.index(car)+1) + "\t"+car+"\n")
+    choice = int(input('Enter Here: '))
+    car = car_list[choice-1]
+    rang = range_list[choice-1]
+    remaining_range = (rang*battery_level)/100
+    
+    return remaining_range, (car, rang)
+
+def getRange(rem_battery):
+    rem, deets = RemainingRange(rem_battery)
+    return rem, deets
