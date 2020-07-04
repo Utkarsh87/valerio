d49cf456119cca0c798feb9ce94f071801ad14d6
Utkarsh Kumar Singh <uks1k999@gmail.com>
2020-07-04 14:26:13 +0530

    OS generalizability introduced

--- Range/RangeCalculation.py
+++ Range/RangeCalculation.py
@@ -1,8 +1,21 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('F:\\Valerio\\Scripts and Plots\\ValerioTrials\\Range\\vehicle_data.csv')
 
+import platform
+if platform.system() == 'Windows':
+    split_by = "\\"
+else:
+    split_by = "/"
+# Note: Modern windows filesystems generally allow both / and \ in their paths and hence they can be used
+# interchangeably, but for the few systems that don't; the above check is necessary.
+
+import os
+cur = os.path.abspath(__file__)
+cur = cur.rsplit(split_by, 1)[0]
+
+
+df = pd.read_csv(cur+split_by+"vehicle_data.csv")
 car_list = list(df['VEHICLE NAME'])
 range_list = list(df['DRIVING RANGE (IN KM)'])
 
